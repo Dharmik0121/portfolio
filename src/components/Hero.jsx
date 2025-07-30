2e597afa7130611952046af970fea799f023c226
Dharmik0121 <104291062+Dharmik0121@users.noreply.github.com>
2025-07-30 10:46:27 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-20 md:gap-4 items-center justify-center">
         <div className="flex-1 tracking-wide space-y-4 md:ml-24">
           <h1 className="text-4xl text-center md:text-left md:text-5xl font-bold">Hi, I am Dharmik Meghani </h1>
-          <TypingText text="Full stack Web Developer" speed={100} />
+          <TypingText texts={["Full Stack Web Developer", "React.js Developer"]} speed={100} />
           <p className="text-base text-center md:text-left text-gray-900 dark:text-gray-400 font-medium">
             I’m a passionate MERN stack developer who loves building real-world applications like invoice generators, billing systems, and authentication platforms. From designing responsive interfaces to integrating complex backend features, I enjoy turning ideas into fast, functional, and user-friendly web solutions.          </p>
           <div className="flex items-center justify-center md:justify-start">
